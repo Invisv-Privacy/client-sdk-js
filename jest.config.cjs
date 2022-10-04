3cfbb3853083f2c8690b0050618c67a9e142d510
Maxb <bittmanmax@gmail.com>
2022-10-04 16:19:26 -0700

    Fix tests by mocking web worker

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,8 @@ module.exports = {
   clearMocks: true,
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   preset: 'ts-jest',
+  moduleNameMapper: {
+    "^web-worker:.*$": '<rootDir>/src/test/worker.ts'
+  },
   testEnvironment: 'node',
 };

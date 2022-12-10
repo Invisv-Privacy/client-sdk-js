d51b73de0dd17e647e3906bd48a8844de08a2d01
raphaelluchini <rluchini@minddoc.de>
2022-12-10 21:56:43 +0100

    Make vite compatible with rollup workers

--- src/room/track/RemoteTrack.ts
+++ src/room/track/RemoteTrack.ts
@@ -4,7 +4,7 @@ import { monitorFrequency } from '../stats';
 import { Track } from './Track';
 import log from '../../logger';
 // @ts-ignore
-import Worker from 'web-worker:../../worker/worker';
+import Worker from '../../worker/worker?worker';
 import { keyRotationMs } from '../defaults';
 
 export default abstract class RemoteTrack extends Track {
